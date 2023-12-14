c9496506bbfc18a54cc855330085443f6c079c28
MamedovaS <119295960+MamedovaS@users.noreply.github.com>
2023-12-14 12:31:56 +0200

    lab6

--- src/components/stadium/stadiums.js
+++ src/components/stadium/stadiums.js
@@ -4,19 +4,19 @@ function Stadiums(){
         <div className="container">
             
             <li className="stadiumitem">
-                <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/e1/f7/d3/visit-wildlights-at-the.jpg?w=500&h=400&s=1" className="stadiumimage"/>
-                <h4 className="stadiumheading">Ohio stadium</h4>
-                <p className="stadiuminfo">The Columbus stadium and Aquarium is located in Powell, Ohio, just north of Columbus. It is one of the largest and most famous stadiums in the United States. The stadium is known for its diverse collection of animals, conservation efforts, and educational programs.</p>
+                <img src="public\logo192.png" className="stadiumimage"/>
+                <h4 className="stadiumheading">same stadium</h4>
+                <p className="stadiuminfo">cool one</p>
             </li>
             <li className="stadiumitem">
-                <img src="https://cdn.britannica.com/14/142114-050-2DEBE25C/entrance-Elephant-Gate-Berlin-stadiumlogical-Garden-and.jpg" className="stadiumimage"/>
-                <h4 className="stadiumheading">Berlin stadium</h4>
-                <p className="stadiuminfo">The Berlin stadium, officially known as the "stadiumlogischer Garten Berlin" in German, is one of the most famous and historic stadiums in the world. Here are some key details about the Berlin stadium:</p>
+                <img src="public\logo192.png" className="stadiumimage"/>
+                <h4 className="stadiumheading">another stadium</h4>
+                <p className="stadiuminfo">not bad</p>
             </li>
             <li className="stadiumitem">
-                <img src="https://images.adsttc.com/media/images/5420/e064/c07a/800d/e500/0092/large_jpg/ARM131211-02.jpg?1411440729" className="stadiumimage"/>
-                <h4 className="stadiumheading">Paris stadium</h4>
-                <p className="stadiuminfo">The Paris stadiumlogical Park, commonly known as the "Parc stadiumlogique de Paris" or "stadium de Vincennes," is a prominent stadium located in the eastern part of Paris, France. Here's some information about the Paris stadium:</p>
+                <img src="public\logo192.png" className="stadiumimage"/>
+                <h4 className="stadiumheading">old stadium</h4>
+                <p className="stadiuminfo">old and grous</p>
             </li>
             
             
