ce76d0629664b21670953eef3e30e929b9199510
MamedovaS <119295960+MamedovaS@users.noreply.github.com>
2023-12-14 09:45:24 +0200

    lab6

---
+++ my-react-app/src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Nav from './components/nav/nav'
+import './components/nav/nav.css'
+import Info from './components/info/info'
+import './components/info/info.css'
+import Stadium from './components/stadiums'
+import './components/studium/stadiums'
+import ViewMoreButton from './components/viewmoreButton/viewmoreButton'
+import './components/viewmoreButton/viewmoreButton.css'
+import Footer from './components/footer/footer'
+import './components/footer/footer.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Info/>
+      <Stadium/>
+      <ViewMoreButton/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/viewmoreButton/viewmoreButton.js
@@ -0,0 +1,10 @@
+
+function ViewMoreButton(){
+    return (
+        <div className="box">     
+            <button className="viewMore">View More</button>
+        </div>
+    );
+}
+
+export default ViewMoreButton
