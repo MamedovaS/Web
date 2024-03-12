2696e04771219b4436061aad9b4e05797e996350
MamedovaS <119295960+MamedovaS@users.noreply.github.com>
2024-03-12 11:17:58 +0200

    awd

--- src/components/catalogSearch/catalogSearch.js
+++ src/components/catalogSearch/catalogSearch.js
@@ -1,29 +1,64 @@
-const CatalogSearch = () => {
-    return (
+import { useState } from "react";
+
+
+const CatalogSearch = ({ applyFilter }) => {
+  const [minTicket, setMinTicket] = useState("");
+  const [maxTicket, setMaxTicket] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleApplyClick = () => {
+    applyFilter(minTicket, maxTicket, sortBy, searchInput);
+  };
+  
+  return (
       <div class="filterwrapper">
         <div class="filterinner">
           <div class="filter-options">
             <div class="dropdown">
               <label for="filter">Filter:</label>
-              <select id="filter">
-                <option>Sort by tickets</option>
-                <option>Sort by price</option>
-                <option>Sort by location</option>
+              <select
+                  id="filter"
+                  value={sortBy}
+                  onChange={(e) => setSortBy(e.target.value)}
+                  className="selectFilter"
+                >
+                <option value="ticket">Sort by ticket</option>
+                <option value="price">Sort by price</option>
+                <option value="location">Sort by location</option>
               </select>
             </div>
-            <p class="sorted-by">Stadiums sorted by: tickets/price/location</p>
           </div>
-          <div class="tickets-inputs">
-            <label for="mintickets">Minimum tickets</label>
-            <input type="text" id="mintickets" name="mintickets" />
-            <label for="maxtickets">Maximum tickets</label>
-            <input type="text" id="maxtickets" name="maxtickets" />
+          <div class="ticket-inputs">
+            <label for="minTicket">Minimum ticket</label>
+            <input type="number"
+              name="minTicket"
+              className="minTicket"
+              value={minTicket}
+              onChange={(e) => setMinTicket(e.target.value)} />
+            
+            <label for="maxTicket">Maximum ticket</label>
+            <input type="number"
+              name="maxTicket"
+              className="maxTicket"
+              value={maxTicket}
+              onChange={(e) => setMaxTicket(e.target.value)}  />
+          <div className="search">
+            <label>
+              Search
+              <input
+                type="text"
+                name="searchInput"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                placeholder="Type something..."
+              />
+            </label>
           </div>
-          <button class="apply-button">Apply</button>
+          </div>
+          <button class="apply-button" onClick={handleApplyClick}>Apply</button>
         </div>
       </div>
     );
   };
-  
-  export default CatalogSearch;
-  
+export default CatalogSearch;
